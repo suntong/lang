a9e103de08e09c1829fe06c8c4c780d101439587
Tong Sun <suntong@cpan.org>
2013-01-17 22:55:33 -0500

    - [+] add ThreadedEchoServer and its test client

---
+++ lang/Go/Examples/net/ThreadedEchoClient.go
@@ -0,0 +1,55 @@
+/* ThreadedEchoClient
+ */
+package main
+
+import (
+	"net"
+	"os"
+	"fmt"
+	"time"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s host:port ", os.Args[0])
+		os.Exit(1)
+	}
+	service := os.Args[1]
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	checkError(err)
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	checkError(err)
+
+	handshake(conn)
+
+	os.Exit(0)
+}
+
+func handshake(conn net.Conn) {
+	// close connection on exit
+	defer conn.Close()
+
+	var buf [512]byte
+	for i := 1; i <= 5; i++ {
+		daytime := time.Now().String()
+		fmt.Fprintf(os.Stderr, "=> %s\n", daytime)
+		conn.Write([]byte(daytime))
+
+		// read upto 512 bytes
+		n, err := conn.Read(buf[0:])
+		checkError(err)
+
+		fmt.Println(string(buf[0:n]))
+
+		time.Sleep(time.Second)
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
