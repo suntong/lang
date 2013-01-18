1c056a291a1dca60e843a8f1e3ace56fdc2702e5
Tong Sun <suntong@cpan.org>
2013-01-18 14:20:03 -0500

    - [+] add SocketServer example, both Blocked and Threaded

---
+++ lang/Go/Examples/net/SocketServerBlocked.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "fmt"
+    "net"
+    "os"
+    "time"
+)
+
+func main() {
+    service := ":7777"
+    tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+    checkError(err)
+    listener, err := net.ListenTCP("tcp", tcpAddr)
+    checkError(err)
+    for {
+        conn, err := listener.Accept()
+        if err != nil {
+            continue
+        }
+        daytime := time.Now().String()
+        conn.Write([]byte(daytime)) // don't care about return value
+        conn.Close()                // we're finished with this client
+    }
+}
+func checkError(err error) {
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+        os.Exit(1)
+    }
+}
