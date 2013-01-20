19d8f36a733fa109d141cde358110bc3706736fc
Tong Sun <suntong@cpan.org>
2013-01-19 22:40:49 -0500

    - [+] add FilepathWalk, filepath Walk demo

---
+++ lang/Go/Examples/sys/FilepathWalk.go
@@ -0,0 +1,37 @@
+////////////////////////////////////////////////////////////////////////////
+// Porgram: FilepathWalk.go
+// Purpose: filepath Walk demo
+// Authors: Tong Sun (c) 2013, All rights reserved
+////////////////////////////////////////////////////////////////////////////
+
+package main
+
+import (
+  "fmt"
+  "os"
+  "path/filepath"
+)
+
+func visit(path string, f os.FileInfo, err error) error {
+  fmt.Printf("  Visiting: %s\n", path)
+  return nil
+}
+
+func main() {
+  if len(os.Args) != 2 {
+    fmt.Println("Usage:\n  ", os.Args[0], "root_path")
+    os.Exit(1)
+  }
+
+  root := os.Args[1]
+  err := filepath.Walk(root, visit)
+  checkError(err)
+}
+
+
+func checkError(err error) {
+    if err != nil {
+        fmt.Println("Fatal error ", err.Error())
+        os.Exit(1)
+    }
+}
