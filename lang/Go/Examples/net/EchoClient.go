2e4e2b9d7884a58ef973c147949f070e4ae8b451
Tong Sun <suntong@cpan.org>
2013-01-20 23:42:55 -0500

    - [*] cosmetic, better usage output

--- lang/Go/Examples/net/EchoClient.go
+++ lang/Go/Examples/net/EchoClient.go
@@ -17,7 +17,7 @@ import (
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Fprintf(os.Stderr, "Usage: %s host:port ", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Usage:\n %s host:port\n", os.Args[0])
 		os.Exit(1)
 	}
 	service := os.Args[1]
