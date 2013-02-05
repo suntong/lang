74502b7600e36bde21aec74cf8ab00f007485661
Tong Sun <suntong@cpan.org>
2013-02-05 11:34:14 -0500

    - [+] StdIO, add stdout & stderr output sequent testing

--- lang/CSharp/Examples/TestConsoleApp/StdIO/Program.cs
+++ lang/CSharp/Examples/TestConsoleApp/StdIO/Program.cs
@@ -18,7 +18,9 @@ namespace StdIO
         static void Main(string[] args)
         {
             TestStdIO();
-            TestSmtp();
+            TestIOSeq1();
+            TestIOSeq2();
+            //TestSmtp();
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
@@ -33,6 +35,41 @@ namespace StdIO
             Console.WriteLine("stdout: " + Guid.NewGuid().ToString()
                 + System.IO.Path.GetTempFileName() + ".bat");
             Console.Error.WriteLine("stderr: " + DateTime.Today);
+        }
+
+        /// <summary>
+        /// TestIOSeq1, Test C# stdout & stderr output sequent 
+        /// </summary>
+        static void TestIOSeq1()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Hello");
+            }
+            Console.Error.WriteLine("Bah bah");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("2");
+            }
+        }
+
+        /// <summary>
+        /// TestIOSeq2, Test C# stdout & stderr output sequent, with flushing
+        /// </summary>
+        static void TestIOSeq2()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Hello");
+            }
+            Console.Out.Flush();
+            Console.Error.WriteLine("Bah bah");
+            Console.Error.Flush();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("2");
+            }
+            Console.Out.Flush();
         }
 
         /// <summary>
