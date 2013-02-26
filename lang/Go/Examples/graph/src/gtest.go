d27d3a9f4e121a8c3a36153abf1b734ef254058a
Tong Sun <suntong@cpan.org>
2013-02-25 23:59:48 -0500

    - [+] GO graph, add cluster/tsgraph.go and provide tests in gtest.go

--- lang/Go/Examples/graph/src/gtest.go
+++ lang/Go/Examples/graph/src/gtest.go
@@ -26,10 +26,7 @@ func parse2graph(filename string) *gographviz.Graph {
   g, err := gographviz.Parse(all) // *ast.Graph
   checkError(err)
   //fmt.Printf("Parsed: %v\n", g)
-  //ag := gographviz.NewAnalysedGraph(g) // *Graph
-  //ag := cluster.NewAnalysedGraph(g) // *Graph
-	ag := gographviz.NewGraph()
-	cluster.Cluster(g, ag)
+  ag := gographviz.NewAnalysedGraph(g) // *Graph
 
   //fmt.Printf("Analysed: %v\n", ag)
   //fmt.Printf("Written: %v\n", ag.String())
@@ -37,14 +34,21 @@ func parse2graph(filename string) *gographviz.Graph {
 }
 
 func main() {
-  if len(os.Args) != 2 {
-    fmt.Fprintf(os.Stderr, "Usage:\n %s graphviz_file\n", os.Args[0])
+  if len(os.Args) < 3 {
+    fmt.Fprintf(os.Stderr, "Usage:\n %s graphviz_file nodeName\n", os.Args[0])
     os.Exit(1)
   }
   gf := os.Args[1]
+  nn := os.Args[2]
 
   //_ = parser2graph(gf)
-  _ = parse2graph(gf)
+  g := parse2graph(gf)
+  cg := cluster.Graph{*g}
+  
+  fmt.Printf("%#v\n", cg.Lookup(nn))
+  fmt.Printf("%#v\n", cg.EdgesToParents(nn))
+  fmt.Printf("%#v\n", cg.EdgesToChildren(nn))
+
   os.Exit(0)
 }
 
