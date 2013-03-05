295af1693d3e6441525e0ce9845dc8ec944e2a68
Tong Sun <suntong@cpan.org>
2013-03-05 18:42:32 -0500

    - [*] Go oo/StructAnonymousFieldMeth2, show that an anonymous pointer in struct is as good as the anonymous field

--- lang/Go/Examples/oo/StructAnonymousFieldMeth2.go
+++ lang/Go/Examples/oo/StructAnonymousFieldMeth2.go
@@ -28,6 +28,11 @@ type Employee struct {
   company string
 }
 
+type Manager struct {
+  *Human   //an anonymous field of type *Human
+  company string
+}
+
 //A human method to say hi
 func (h *Human) SayHi() {
   fmt.Printf("Hi, I am %s you can call me on %s\n", h.name, h.phone)
@@ -42,7 +47,9 @@ func (e *Employee) SayHi() {
 func main() {
   mark := Student{Human{"Mark", 25, "222-222-YYYY"}, "MIT"}
   sam := Employee{Human{"Sam", 45, "111-888-XXXX"}, "Golang Inc"}
+  tom := Manager{&Human{"Tom", 55, "222-999-XXXX"}, "Golang Inc"}
 
   mark.SayHi()
   sam.SayHi()
+  tom.SayHi()
 }
