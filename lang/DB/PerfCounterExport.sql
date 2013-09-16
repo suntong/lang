f7455390e1939f4401293c69ed406fe6a2635267
Tong Sun <suntong@cpan.org>
2013-09-16 11:59:14 -0400

    - [+] add PerfCounterExport, for export the Load Test perfmon counter readings

---
+++ lang/DB/PerfCounterExport.sql
@@ -0,0 +1,102 @@
+/*== TSL_prc_PerfCounterCollectionInCsvFormat ===================================
+* Returns a list of perfmon counter names and values in a format that can be written
+* to a CSV file for import into Perfmon.
+* EXAMPLE:
+EXEC   TSL_prc_PerfCounterCollectionInCsvFormat
+              @RunId = N'9',
+              @InstanceName = N'\\controller\%'
+===============================================================================*/
+
+/*********************************************************
+// Copyright (c) Microsoft. All rights reserved.
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+// Code initially written by Geoff Gray.
+// http://blogs.msdn.com/b/geoffgr/archive/2013/09/09/
+**********************************************************/
+
+--START CODE--
+CREATE PROCEDURE TSL_prc_PerfCounterCollectionInCsvFormat
+       @RunId nvarchar(10),
+       @InstanceName nvarchar(1024)
+AS
+       DECLARE @CounterName nvarchar(max), @CounterNameColumns nvarchar(max)
+       --Get List of columns to use in query. Shove them into a single long XML string
+       SELECT @CounterNameColumns = (
+                                  SELECT ', [' + REPLACE(InstanceName, ']', ']]') + ']' FROM MTSL_View_PerfmonInstanceNamesAndIds
+                                  WHERE LoadTestRunId = @RunId
+                                  AND InstanceName LIKE @InstanceName
+                     FOR XML PATH(''))
+       --Make a copy of the list WITHOUT the comma at the very beginning of the string
+       SELECT @CounterName = RIGHT(@CounterNameColumns, LEN(@CounterNameColumns) - 1)
+       -- Use the previous strings to build the query string that can be pivoted
+       DECLARE @SQL nvarchar(max)
+        SELECT @SQL = N'
+              select
+              IntervalStartTime AS [(PDH-CSV 4.0) (Eastern Daylight Time)(240)]' +
+          --IntervalStartTime' +
+              @CounterNameColumns + '
+        from (
+                     select
+                           interval.IntervalStartTime,
+                           MTSL_View_PerfmonInstanceNamesAndIds.InstanceName,
+                           countersample.ComputedValue
+                     FROM
+                           MTSL_View_PerfmonInstanceNamesAndIds
+                           INNER JOIN LoadTestPerformanceCounterSample AS countersample
+                           ON countersample.InstanceId = MTSL_View_PerfmonInstanceNamesAndIds.InstanceId
+                           AND countersample.LoadTestRunId = MTSL_View_PerfmonInstanceNamesAndIds.LoadTestRunId
+                           INNER JOIN LoadTestRunInterval AS interval
+                           ON interval.LoadTestRunId = countersample.LoadTestRunId
+                           AND interval.TestRunIntervalId = countersample.TestRunIntervalId
+                     WHERE
+                           MTSL_View_PerfmonInstanceNamesAndIds.LoadTestRunId = ' + @RunId + '
+                           AND
+                           MTSL_View_PerfmonInstanceNamesAndIds.InstanceName LIKE '''+@InstanceName+'''
+        ) Data
+        PIVOT (
+          SUM(ComputedValue)
+          FOR InstanceName
+          IN (
+               ' + @CounterName + '
+          )
+        ) PivotTable
+        ORDER BY IntervalStartTime ASC
+        '
+        print @SQL
+       -- Execute the generated query
+       exec sp_executesql @SQL
+GO
+--START CODE--
+
+GRANT EXECUTE ON TSL_prc_PerfCounterCollectionInCsvFormat TO PUBLIC
+GO
+
+/*===============================================================================
+MTSL_View_PerfmonInstanceNamesAndIds
+===============================================================================*/
+
+CREATE VIEW MTSL_View_PerfmonInstanceNamesAndIds AS
+       SELECT
+              instance.LoadTestRunId
+              ,instance.InstanceId
+              ,(
+                     '\\' + category.MachineName
+                     + '\' + category.CategoryName
+                     +      case instance.InstanceName when 'systemdiagnosticsperfcounterlibsingleinstance'
+                                   then ''
+                                   else '(' + instance.InstanceName  + ')'
+                                   end
+                     + '\' + counter.CounterName
+              ) AS InstanceName
+       FROM LoadTestPerformanceCounterCategory AS category
+       INNER JOIN LoadTestPerformanceCounter AS counter
+              ON category.LoadTestRunId = counter.LoadTestRunId
+              AND category.CounterCategoryId = counter.CounterCategoryId
+       INNER JOIN LoadTestPerformanceCounterInstance AS instance
+              ON counter.CounterId = instance.CounterId
+              AND counter.LoadTestRunId = instance.LoadTestRunId
+GO
