966b6245d78fc9449090d4fa026df8f7f3a5776e
Tong Sun <suntong@cpan.org>
2014-05-26 11:26:03 -0400

    - [+] add ListSoftwareInstalled.ps1

---
+++ lang/PowerShell/Examples/Sys/ListSoftwareInstalled.ps1
@@ -0,0 +1,8 @@
+﻿# Get a List of Installed Software
+# http://www.howtogeek.com/165293/how-to-get-a-list-of-software-installed-on-your-pc-with-a-single-command/
+# http://blogs.msdn.com/b/powershell/archive/2009/11/15/i-can-do-that-with-1-line-of-powershell-installed-software.aspx
+
+$wp = Get-WmiObject -Class Win32_Product | Select-Object Name, Vendor, Version  | Sort-Object Vendor, Name
+$wp | export-csv AppList.csv
+$wp | ft -auto > AppList.txt
+# $wp | ogv
