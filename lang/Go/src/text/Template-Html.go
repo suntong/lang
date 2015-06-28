3b071ec24bd3379b034f2fe035c480fcc620a1b9
Tong Sun <suntong@cpan.org>
2015-06-27 22:28:16 -0400

    - [+] add Template-Html, Go html template usage demo

---
+++ lang/Go/src/text/Template-Html.go
@@ -0,0 +1,51 @@
+////////////////////////////////////////////////////////////////////////////
+// Porgram: Template-Html
+// Purpose: Go html template usage demo
+// Authors: Tong Sun (c) 2015, All rights reserved
+// Based on https://www.socketloop.com/references/golang-html-template-parsefiles-function-examples
+////////////////////////////////////////////////////////////////////////////
+
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"os"
+)
+
+func main() {
+
+	data := map[string]interface{}{
+		"Title": "Hello World!",
+	}
+
+	t := template.New("HELLO")
+
+	var templates = template.Must(t.ParseFiles("Template-Html.tmpl"))
+	err := templates.ExecuteTemplate(os.Stdout, "Template-Html.tmpl", data)
+	// working
+	fmt.Println("\n\n")
+
+	// This option, need to use the template file twice, once for
+	// `ParseFiles`, then for `ExecuteTemplate`. That looks awkward and
+	// unnecessary. The following one looks straightforward to me.
+
+	t = template.New("Test template")
+	t, err = t.ParseFiles("Template-Html.tmpl")
+	// not working!
+	t, err = t.Parse("<title>{{ .Title }}</title>")
+	// working
+	checkError(err)
+
+	err = t.Execute(os.Stdout, data)
+
+	fmt.Println("\n\nTemplate name is : ", t.Name())
+
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Fatal error ", err.Error())
+		os.Exit(1)
+	}
+}
