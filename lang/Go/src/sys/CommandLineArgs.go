ae4d36b1ea8304385d83b238da95cb29489ab90f
Tong Sun <suntong@cpan.org>
2015-07-27 16:50:24 -0400

    - [*] CommandLineArgs add Args loop output

--- lang/Go/src/sys/CommandLineArgs.go
+++ lang/Go/src/sys/CommandLineArgs.go
@@ -9,23 +9,28 @@
 package main
 
 import (
-	//	"fmt"
+	"fmt"
 	"os"
 	"path/filepath"
 )
 
 func main() {
-	println("I am ", os.Args[0])
+	println("I am", os.Args[0])
 
 	baseName := filepath.Base(os.Args[0])
-	println("The base name is ", baseName)
+	println("The base name is", baseName)
 
 	// The length of array a can be discovered using the built-in function len
-	println("Argument # is ", len(os.Args))
+	println("Argument # is", len(os.Args))
 
 	// the first command line arguments
 	if len(os.Args) > 1 {
 		println("The first command line argument: ", os.Args[1])
+	}
+
+	println("")
+	for i, v := range os.Args {
+		fmt.Printf("Args %d: %s\n", i, v)
 	}
 }
 
