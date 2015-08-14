084b57110bb2db603c54ef470400510a9776b9bf
Tong Sun <suntong@cpan.org>
2015-08-13 23:45:08 -0400

    - [*] better FilepathWalk demo

--- lang/Go/src/sys/FilepathWalk.go
+++ lang/Go/src/sys/FilepathWalk.go
@@ -7,31 +7,34 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "path/filepath"
+	"fmt"
+	"os"
+	"path/filepath"
 )
 
 func visit(path string, f os.FileInfo, err error) error {
-  fmt.Printf("  Visiting: %s\n", path)
-  return nil
+	if f.IsDir() {
+		fmt.Printf("  Directory: %s\n", path)
+	} else {
+		fmt.Printf("  File: %s with %d bytes\n", path, f.Size())
+	}
+	return nil
 }
 
 func main() {
-  if len(os.Args) != 2 {
-    fmt.Println("Usage:\n  ", os.Args[0], "root_path")
-    os.Exit(1)
-  }
+	if len(os.Args) != 2 {
+		fmt.Println("Usage:\n  ", os.Args[0], "root_path")
+		os.Exit(1)
+	}
 
-  root := os.Args[1]
-  err := filepath.Walk(root, visit)
-  checkError(err)
+	root := os.Args[1]
+	err := filepath.Walk(root, visit)
+	checkError(err)
 }
 
-
 func checkError(err error) {
-    if err != nil {
-        fmt.Println("Fatal error ", err.Error())
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Println("Fatal error ", err.Error())
+		os.Exit(1)
+	}
 }
