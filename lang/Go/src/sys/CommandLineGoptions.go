2adc4a10d4221268ddb0d3c2aeae9322c0dab357
Tong Sun <suntong@cpan.org>
2015-10-22 14:54:46 -0400

    - [*] complete demo for goptions from voxelbrain

--- lang/Go/src/sys/CommandLineGoptions.go
+++ lang/Go/src/sys/CommandLineGoptions.go
@@ -1,21 +1,25 @@
 package main
 
 import (
-	"github.com/voxelbrain/goptions"
+	"fmt"
 	"os"
 	"time"
 )
 
-func main() {
-	options := struct {
+import (
+	"github.com/voxelbrain/goptions"
+)
+
+var (
+	options = struct {
 		Server   string        `goptions:"-s, --server, obligatory, description='Server to connect to'"`
 		Password string        `goptions:"-p, --password, description='Don\\'t prompt for password'"`
 		Timeout  time.Duration `goptions:"-t, --timeout, description='Connection timeout in seconds'"`
 		Help     goptions.Help `goptions:"-h, --help, description='Show this help'"`
 
-		goptions.Verbs
+		Verb    goptions.Verbs
 		Execute struct {
-			Command string   `goptions:"--command, mutexgroup='input', description='Command to exectute', obligatory"`
+			Command string   `goptions:"-c, --command, mutexgroup='input', description='Command to exectute', obligatory"`
 			Script  *os.File `goptions:"--script, mutexgroup='input', description='Script to exectute', rdonly"`
 		} `goptions:"execute"`
 		Delete struct {
@@ -25,11 +29,17 @@ func main() {
 	}{ // Default values goes here
 		Timeout: 10 * time.Second,
 	}
+)
+
+func main() {
 	goptions.ParseAndFail(&options)
+	fmt.Printf("Selected verb: %s\n", options.Verb)
+	fmt.Printf("Execute.Command: %s\n", options.Execute.Command)
 }
 
 /*
 
+$ go run CommandLineGoptions.go
 Error: --server must be specified
 Usage: CommandLineGoptions [global options] <verb> [verb options]
 
@@ -44,9 +54,16 @@ Verbs:
         -n, --name     Name of the entity to be deleted (*)
         -f, --force    Force removal
     execute:
-            --command  Command to exectute (*)
+        -c, --command  Command to exectute (*)
             --script   Script to exectute
 exit status 1
 
+$ go run CommandLineGoptions.go -s sss execute -c 'test'
+Selected verb: execute
+Execute.Command: test
+
+$ go run CommandLineGoptions.go -s sss execute -c 'test' --script CommandLineGoptions.go
+Error: Exactly one of --command, --script must be specified
+Usage: CommandLineGoptions ...
 
 */
