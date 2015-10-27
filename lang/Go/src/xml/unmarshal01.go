44540ef0114f402b4a28d00ab865a8d0a7d0442e
Tong Sun <suntong@cpan.org>
2015-10-27 17:40:27 -0400

    - [+] unmarshal01.go add printXML, not working for Name and Company

--- lang/Go/src/xml/unmarshal01.go
+++ lang/Go/src/xml/unmarshal01.go
@@ -23,6 +23,7 @@ func main() {
 	type Result struct {
 		XMLName xml.Name `xml:"Person"`
 		Name    string   `xml:"FullName"`
+		Company string   `xml:"Company"`
 		Phone   string
 		Email   []Email
 		Groups  []string `xml:"Group>Value"`
@@ -33,7 +34,10 @@ func main() {
 	data := `
 		<Person>
 			<FullName>Grace R. Emlin</FullName>
-			<Company>Example Inc.</Company>
+			<Company>
+			 <Name>Example Inc.</Name>
+			 <Addr>Example work place</Addr>
+			</Company>
 			<Email where="home">
 				<Addr>gre@example.com</Addr>
 			</Email>
@@ -53,10 +57,24 @@ func main() {
 		fmt.Printf("error: %v", err)
 		return
 	}
+
+	//printXML("As XML:", v)
+	printXML("Name As XML:", v.Name)
+	printXML("Email As XML:", v.Email)
+	printXML("Groups As XML:", v.Groups)
+	printXML("Company As XML:", v.Company)
+
 	fmt.Printf("XMLName: %#v\n", v.XMLName)
 	fmt.Printf("Name: %q\n", v.Name)
 	fmt.Printf("Phone: %q\n", v.Phone)
 	fmt.Printf("Email: %v\n", v.Email)
 	fmt.Printf("Groups: %v\n", v.Groups)
 	fmt.Printf("Address: %v\n", v.Address)
+}
+
+func printXML(s string, v interface{}) {
+	fmt.Println(s)
+	b, _ := xml.MarshalIndent(v, "", "\t")
+	fmt.Println(string(b))
+	fmt.Println()
 }
