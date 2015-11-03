e4380990f44861689d306c563f4582a971c16524
Tong Sun <suntong@cpan.org>
2015-11-03 14:30:33 -0500

    - [*] make it really decode UTF16 sequences to ASCII string

--- lang/Go/src/text/utf16B.go
+++ lang/Go/src/text/utf16B.go
@@ -4,13 +4,16 @@
 // Authors: Tong Sun (c) 2015, All rights reserved
 // Credits: https://golang.org/pkg/encoding/base64/#example_Encoding_DecodeString
 //          http://www.pocketsoap.com/pocketsoap/docs/master/headers.htm
+//          http://play.golang.org/p/cCufRfEQso by Jakob Borg
 ////////////////////////////////////////////////////////////////////////////
 
 package main
 
 import (
 	"encoding/base64"
+	"encoding/binary"
 	"fmt"
+	"unicode/utf16"
 )
 
 func main() {
@@ -23,9 +26,15 @@ AFMAdAB1AGYAZgA+ADwALwBkADoARABvAFMAdAB1AGYAZgA+AAoAPAAvAFMAOgBCAG8AZAB5AD4A
 PAAvAFMAOgBFAG4AdgBlAGwAbwBwAGUAPgA=`
 
 	uDec, _ := base64.StdEncoding.DecodeString(uEnc)
-	fmt.Println(uDec)
-	fmt.Println(string(uDec))
+	fmt.Printf("%q\n", uDec)
 
+	u16s := make([]uint16, len(uDec)/2)
+	for i := range u16s {
+		u16s[i] = binary.LittleEndian.Uint16([]byte(uDec[i*2:]))
+	}
+
+	str := string(utf16.Decode(u16s))
+	fmt.Printf("%q\n", str)
 }
 
 /*
