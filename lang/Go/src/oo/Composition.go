9809d08bf50df92e28ee0dfb04a23c81402db72c
Tong Sun <suntong@cpan.org>
2015-11-29 10:12:00 -0500

    - [+] add Go composition & field promotion demo

---
+++ lang/Go/src/oo/Composition.go
@@ -0,0 +1,58 @@
+////////////////////////////////////////////////////////////////////////////
+// Porgram: Composition.go
+// Purpose: Go composition & field promotion demo
+// Authors: Tong Sun (c) 2015, All rights reserved
+// Credits: Richard Sugg, ArdanLabs (Bill Kennedy)
+//          https://gist.github.com/rsperl/01d7a4da7c01706d50f6
+//          https://groups.google.com/d/msg/golang-nuts/uj7qH2qzD1E/Xq1Wya0tBQAJ
+////////////////////////////////////////////////////////////////////////////
+
+// I thought I needed to do inheritance, but given that golang doesn't
+// support inheritance, I knew I wasn't thinking in go idioms. The solution
+// is composition, and it's made possible by method and field promotion
+
+// For e.g., say I have a type User with some methods applicable to all Users:
+
+package user
+
+type User struct {
+	Name  string
+	Email string
+}
+
+func (u *User) SendEmail(subject, body string) bool {
+	// ... send an email
+	return true
+}
+
+// You may also have an Admin, who is a User, but has additional abilities
+// and fields not applicable to ordinary users. How do you add the
+// additional properties and methods to an Admin while retaining the
+// original implementation of a User? The answer is in composition. When a
+// type is embedded in another type, the fields and methods of the subtype
+// are automatically promoted to the type that is embedding. Think of it as
+// having "super" being called automatically.
+
+// Because Admin embeds User, Admin now has all the properties of User as well
+// as the additional ones defined specifically for Admin. In other words, given
+// "adm" is of type *Admin, you can do
+//
+// Call the User's Name property and methods explicitly
+//   adm.User.Name
+//   adm.User.SendEmail("hi", "there")
+// Or take advantage of type promotion
+//   adm.Name
+//   adm.SendEmail("hi", "there")
+// Likewise, you also have Admin's own properties that are not
+// avaiable at all to type User
+//   adm.UnlockGate()
+
+type Admin struct {
+	*User
+	SecretPassword string
+}
+
+func (a *Admin) UnlockGate() bool {
+	// ... do admin stuff
+	return true
+}
