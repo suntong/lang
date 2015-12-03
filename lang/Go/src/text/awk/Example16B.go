8c09785a90ca384f9473154f5782488f17b63fb2
Tong Sun <suntong@cpan.org>
2015-12-02 22:14:46 -0500

    - [+] add awk demo Example16

---
+++ lang/Go/src/text/awk/Example16B.go
@@ -0,0 +1,44 @@
+////////////////////////////////////////////////////////////////////////////
+// Purpose: Go awk demo
+// Authors: Tong Sun (c) 2015, All rights reserved
+////////////////////////////////////////////////////////////////////////////
+
+package main
+
+import (
+	"github.com/spakin/awk"
+	"os"
+)
+
+func main() {
+	s := awk.NewScript()
+
+	s.State = s.NewValue("")
+	s.AppendStmt(func(s *awk.Script) bool { return !s.F(1).StrEqual(s.State) },
+		func(s *awk.Script) {
+			//s.Println()
+			s.State = s.F(1)
+			println(s.State)
+		})
+
+	if err := s.Run(os.Stdin); err != nil {
+		panic(err)
+	}
+}
+
+/*
+
+Example (16)
+https://godoc.org/github.com/spakin/awk
+
+Write all lines whose first field is different from the previous line's
+(AWK: $1 != prev {print; prev = $1}).
+
+$ seq 5 | go run Example16A.go
+1
+2
+3
+4
+5
+
+*/
