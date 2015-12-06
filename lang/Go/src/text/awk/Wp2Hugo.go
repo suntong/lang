a8edc36a61163aa71aa79570349056f688016c36
Tong Sun <suntong@cpan.org>
2015-12-06 16:40:24 -0500

    - [*] switched Wp2Hugo from TOML format to YAML

--- lang/Go/src/text/awk/Wp2Hugo.go
+++ lang/Go/src/text/awk/Wp2Hugo.go
@@ -29,13 +29,12 @@ E.g., for an input of wordpress's markdown like this,
 
 The output Hugo's meta data is:
 
-    +++
-    title = "Dbab From Start To Finish"
-    date = ["2015-12-05T23:29:19-05:00"]
-    categories = ["Tech"]
-    tags = ["Debian","Ubuntu","Linux","DHCP","DNS","WPAD","dnsmasq","dbab"]
-    menu = ""
-    +++
+    ---
+    title: "Dbab From Start To Finish"
+    date: "2015-12-06T09:57:45-05:00"
+    categories: ["Tech"]
+    tags: ["Debian","Ubuntu","Linux","DHCP","DNS","WPAD","dnsmasq","dbab"]
+    ---
 
 Usage:
 
@@ -59,16 +58,16 @@ func main() {
 			s.Next()
 		})
 
-	// /^ *\[category/ { convert meta data format }
-	s.AppendStmt(func(s *awk.Script) bool { return s.F(0).Match("^ *\\[category") },
+	// /^\[category/ { convert meta data format }
+	s.AppendStmt(func(s *awk.Script) bool { return s.F(0).Match("^\\[category") },
 		func(s *awk.Script) {
 			re := regexp.MustCompile("^# +(.*)$")
 			fmt.Println(re.ReplaceAllString(s.State.(*awk.Value).String(),
-				"+++\ntitle = \"$1\""))
-			fmt.Printf("date = [\"%s\"]\n", time.Now().Format(time.RFC3339))
-			fmt.Printf("categories = [\"%s\"]\n", s.F(3))
-			fmt.Printf("%s = [\"%s\"]\n", s.F(5), s.F(6))
-			fmt.Println("menu = \"\"\n+++\n")
+				"---\ntitle: \"$1\""))
+			fmt.Printf("date: \"%s\"\n", time.Now().Format(time.RFC3339))
+			fmt.Printf("categories: [\"%s\"]\n", s.F(3))
+			fmt.Printf("%s: [\"%s\"]\n", s.F(5), s.F(6))
+			fmt.Println("---\n")
 			//s.Exit()
 			s.Next()
 		})
