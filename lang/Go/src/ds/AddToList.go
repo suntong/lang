5d905d08424a8805310329dfe06edcc89848decb
Tong Sun <suntong@cpan.org>
2015-12-07 19:43:51 -0500

    - [+] add ds/AddToList, Adding elements to a slice

---
+++ lang/Go/src/ds/AddToList.go
@@ -0,0 +1,33 @@
+////////////////////////////////////////////////////////////////////////////
+// Porgram: Composition.go
+// Purpose: Go composition & field promotion demo
+// Authors: Tong Sun (c) 2015, All rights reserved
+// Credits: A Tour of Go
+//          https://tour.golang.org/moretypes/14
+////////////////////////////////////////////////////////////////////////////
+
+package main
+
+import "fmt"
+
+func main() {
+	var a []int
+	printSlice("a", a)
+
+	// append works on nil slices.
+	a = append(a, 0)
+	printSlice("a", a)
+
+	// the slice grows as needed.
+	a = append(a, 1)
+	printSlice("a", a)
+
+	// we can add more than one element at a time.
+	a = append(a, 2, 3, 4)
+	printSlice("a", a)
+}
+
+func printSlice(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n",
+		s, len(x), cap(x), x)
+}
