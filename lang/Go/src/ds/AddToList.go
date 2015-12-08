845738b048971cc1f59f6c50de6426829b5b75d5
Tong Sun <suntong@cpan.org>
2015-12-07 19:52:27 -0500

    - [*] refactoring

--- lang/Go/src/ds/AddToList.go
+++ lang/Go/src/ds/AddToList.go
@@ -11,23 +11,27 @@ package main
 import "fmt"
 
 func main() {
+	intSliceTest()
+}
+
+func intSliceTest() {
 	var a []int
-	printSlice("a", a)
+	printIntSlice("a", a)
 
 	// append works on nil slices.
 	a = append(a, 0)
-	printSlice("a", a)
+	printIntSlice("a", a)
 
 	// the slice grows as needed.
 	a = append(a, 1)
-	printSlice("a", a)
+	printIntSlice("a", a)
 
 	// we can add more than one element at a time.
 	a = append(a, 2, 3, 4)
-	printSlice("a", a)
+	printIntSlice("a", a)
 }
 
-func printSlice(s string, x []int) {
+func printIntSlice(s string, x []int) {
 	fmt.Printf("%s len=%d cap=%d %v\n",
 		s, len(x), cap(x), x)
 }
