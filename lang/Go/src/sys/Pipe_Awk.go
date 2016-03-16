3099d290dbf07d8be45d046262df647777567ac0
Tong Sun <suntong@cpan.org>
2016-03-15 22:25:05 -0400

    - [+] add Pipe_Awk.go, Go pipeline + awk demo

---
+++ lang/Go/src/sys/Pipe_Awk.go
@@ -0,0 +1,64 @@
+////////////////////////////////////////////////////////////////////////////
+// Purpose: Go pipeline + awk demo
+// Authors: Tong Sun (c) 2016, All rights reserved
+// Credits: http://labix.org/pipe, https://github.com/spakin/awk/
+////////////////////////////////////////////////////////////////////////////
+
+package main
+
+import (
+	"fmt"
+)
+
+import (
+	"github.com/spakin/awk"
+	"gopkg.in/pipe.v2"
+)
+
+////////////////////////////////////////////////////////////////////////////
+// Function definitions
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+// Function main
+
+func main() {
+	TestExtending()
+}
+
+// Awk1 writes the first two fields in opposite order if the second field does
+// not match the regular expression "4:" and the first field does not match "7"
+// (AWK: $2 !~ /4:/ && $1 !~ /7/ {print $2, $1}).
+func Awk1() pipe.Pipe {
+	return pipe.TaskFunc(func(st *pipe.State) error {
+		s := awk.NewScript()
+		s.AppendStmt(func(s *awk.Script) bool {
+			return !s.F(2).Match("4:") && !s.F(1).Match("7")
+		}, func(s *awk.Script) { s.Println(s.F(2), s.F(1)) })
+		return s.Run(st.Stdin)
+	})
+}
+
+func TestExtending() {
+	p := pipe.Line(
+		pipe.System("seq 8 | cat -n | sed 's/$/:/'"),
+		Awk1(),
+	)
+	output, err := pipe.Output(p)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+	fmt.Printf("%s", output)
+}
+
+/*
+
+Output:
+
+1: 1
+2: 2
+3: 3
+5: 5
+6: 6
+8: 8
+
+*/
