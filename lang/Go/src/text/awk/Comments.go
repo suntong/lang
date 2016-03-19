52e70d6cd9e52f442029cfd27bb89a98b20bef93
Tong Sun <suntong@cpan.org>
2016-03-18 22:19:39 -0400

    - [+] add Comments.go demo

---
+++ lang/Go/src/text/awk/Comments.go
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////////////////////////////
+// Purpose: Go awk demo
+// Authors: Tong Sun (c) 2015, All rights reserved
+////////////////////////////////////////////////////////////////////////////
+
+package main
+
+import (
+	"github.com/spakin/awk"
+	"os"
+)
+
+func main() {
+	s := awk.NewScript()
+	s.MaxRecordSize = 24 * 1024 * 1024
+
+	s.AppendStmt(func(s *awk.Script) bool {
+		return s.F(1).Match("<Comment")
+	}, func(s *awk.Script) {})
+	// 1; # i.e., print all
+	s.AppendStmt(nil, nil)
+
+	if err := s.Run(os.Stdin); err != nil {
+		panic(err)
+	}
+}
