8b743e4b035d16153845f26488a250ac0dab8d9d
Tong Sun <suntong@cpan.org>
2016-05-28 09:41:52 -0400

    - [+] add ParseAddress.go

---
+++ lang/Go/src/net/email/ParseAddress.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/mail"
+)
+
+func main() {
+	ParseAddress()
+	ParseAddressList()
+}
+
+func ParseAddress() {
+	e, err := mail.ParseAddress("Alice <alice@example.com>")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(e.Name, e.Address)
+
+}
+
+func ParseAddressList() {
+	const list = `Alice <alice@example.com>, "Bob Uncle" <bob@example.com>, "Yang-Tan, Eve" <eve@example.com>`
+	emails, err := mail.ParseAddressList(list)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, v := range emails {
+		fmt.Println(v.Name, "=>", v.Address)
+	}
+
+}
