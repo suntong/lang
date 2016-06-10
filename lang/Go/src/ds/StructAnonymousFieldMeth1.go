64afcad4a40313f006582d544af414a904518212
Tong Sun <suntong@cpan.org>
2016-06-10 12:37:18 -0400

    - [+] add example to "up-size"

--- lang/Go/src/ds/StructAnonymousFieldMeth1.go
+++ lang/Go/src/ds/StructAnonymousFieldMeth1.go
@@ -1,10 +1,8 @@
 ////////////////////////////////////////////////////////////////////////////
 // Porgram: StructAnonymousFieldMeth1
 // Purpose: Go struct anonymous fields methods demo
-// Authors: Tong Sun (c) 2013, All rights reserved
+// Authors: Tong Sun (c) 2013-2016, All rights reserved
 ////////////////////////////////////////////////////////////////////////////
-
-// Style: gofmt -tabs=false -tabwidth=2
 
 // By Big Yuuta, http://go-book.appspot.com/more-methods.html
 
@@ -13,30 +11,38 @@ package main
 import "fmt"
 
 type Human struct {
-  name  string
-  age   int
-  phone string //his own mobile number
+	name  string
+	age   int
+	phone string //his own mobile number
 }
 
 type Student struct {
-  Human  //an anonymous field of type Human
-  school string
+	Human  //an anonymous field of type Human
+	school string
 }
 
 type Employee struct {
-  Human   //an anonymous field of type Human
-  company string
+	Human   //an anonymous field of type Human
+	company string
 }
 
 //A human method to say hi
 func (h *Human) SayHi() {
-  fmt.Printf("Hi, I am %s you can call me on %s\n", h.name, h.phone)
+	fmt.Printf("Hi, I am %s you can call me on %s\n", h.name, h.phone)
 }
 
 func main() {
-  mark := Student{Human{"Mark", 25, "222-222-YYYY"}, "MIT"}
-  sam := Employee{Human{"Sam", 45, "111-888-XXXX"}, "Golang Inc"}
+	mark := Student{Human{"Mark", 25, "222-222-YYYY"}, "MIT"}
+	sam := Employee{Human{"Sam", 45, "111-888-XXXX"}, "Golang Inc"}
 
-  mark.SayHi()
-  sam.SayHi()
+	mark.SayHi()
+	sam.SayHi()
+
+	h1 := Human{"Tom", 25, "222-333-YYYY"}
+	var s1 Student
+	// X: s1 = Student{h1} too few values in struct initializer
+	s1 = Student{h1, "MIT"}
+	// X: s1 = Student{h1, school: "MIT"} mixture of field:value and value
+	s1 = Student{Human: h1, school: "MIT"}
+	s1.SayHi()
 }
