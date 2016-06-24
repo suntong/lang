3cbdd3a2809b5cc4d97fe259c321f119d6fd6bee
Tong Sun <suntong@cpan.org>
2016-06-24 09:53:34 -0400

    - [+] add followup Q&A

--- lang/Go/src/ds/Map-Set1.go
+++ lang/Go/src/ds/Map-Set1.go
@@ -2,7 +2,7 @@
 // Porgram: Set by map
 // Purpose: Demo the SET data structure in GO
 // Authors: Tong Sun (c) 2016, All rights reserved
-// Credits: Val @golang-nuts, https://play.golang.org/p/_n56yMhlRt
+// Credits: Valentin Deleplace, https://play.golang.org/p/_n56yMhlRt
 ////////////////////////////////////////////////////////////////////////////
 
 /*
@@ -50,5 +50,52 @@ Output:
 map[aa:{} bb:{} cc:{}]
 true
 false
+
+*/
+
+/*
+
+> > map is a reference type
+>
+> Does that means that, this function
+>
+>     func (s set) Has(a string) bool { _, ok := s[a]; return ok }
+>
+> is exactly the same as the following?
+>
+>     func (s *set) Has(a string) bool { _, ok := (*s)[a]; return ok }
+>
+> I.e., even the set is super super big, there is no pass-by-value penalty in the first version?
+
+No penalty, passing a map around never copies the values.
+
+But a "pointer to a pointer" receiver is not strictly the same as a pointer receiver.
+
+Valentin Deleplace
+
+*/
+
+/*
+
+> Since a map is a pointer, for any function that take map as a parameter, or function like,
+>
+>  func (s set) Something(xxx)
+>
+> Does it means the map values can be changed inside the function?
+>
+> If yes, what's the proper way to declare it so as to make sure the values cannot be changed inside the function?
+
+Good question, "by reference" vs "by values" is a fundamental question in most languages, and the source of many headaches when used incorrectly so it's worth taking some time to ask and get it right.
+
+Yes you may change the values of an existing map, inside any function or method.
+
+Go doesn't provide idiomatic ways to have "immutable-like" or "view-like" builtin containers, except string which are always immutable.  When you have an array, or a slice, or a map, you can always alter its content.
+
+Having said this, if you don't want to caller to modify the data, you may :
+
+- clone the data (make a copy of the map, you have to explicitly loop for that) and give the clone to the caller. This is called "defensive copying".
+- or create custom struct type, containing unexported map, with exported read-only accessors e.g.   func (c *MyCustomContainer) Get(key string) Value
+
+Valentin Deleplace
 
 */
