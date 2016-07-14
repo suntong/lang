373e151aa73903de5468d193b0837bc6a2281d69
Tong Sun <suntong@cpan.org>
2016-07-14 18:10:04 -0400

    - [+] pandas_MultiIndex add Conditionally set pandas dataframe column values demo

--- lang/Python/pandas_MultiIndex.py
+++ lang/Python/pandas_MultiIndex.py
@@ -68,3 +68,62 @@ print(x.A + ' - ' + x.B)
 
 x.B = x.A + ' - ' + x.B
 print(x)
+
+# Conditionally set pandas dataframe column values
+# http://stackoverflow.com/questions/38228082/conditionally-set-pandas-dataframe-column-values
+
+df = pd.DataFrame({'data1': np.random.randn(100),'data2': np.random.randn(100)})
+print(df.head())
+
+Col = 'data1'
+print(df[Col].head())
+df.data1 = df.data1 +.1
+print(df[Col].head())
+#df.Col = df.Col + 1
+df[Col] = df[Col] + .1
+print(df[Col].head())
+print("\nBetween .25 & .35")
+print(df[(df[Col] >=.25) & (df[Col] <= .35)])
+df[(df[Col] >=.25) & (df[Col] <= .35)] = df[(df[Col] >=.25) & (df[Col] <= .35)]+.1 
+print("Between .35 & .45")
+print(df[(df[Col] >=.35) & (df[Col] <= .45)])
+#gp = df.groupby(level=('data1', 'data2'))
+# TypeError: unorderable types: tuple() > int()
+
+df = pd.DataFrame({'data1': np.random.choice(['foo', 'bar'], 20),
+                   'data2': np.random.choice(['I1', 'I2', 'I3', 'I4'], 20),
+                   'data3': np.random.randn(20)})
+print(df)
+
+Col = 'data1'
+print(df[Col].head())
+df[Col] = df[Col] + "1"
+print(df[Col].head())
+
+#means.loc[w.female != 'female', 'female'] = 0
+
+# https://gist.github.com/suntong/8740c0b6a67cfc79856b
+# Generate the data
+ix3 = pd.MultiIndex.from_arrays([['a', 'a', 'a', 'a', 'b', 'b', 'b', 'b'], ['foo', 'foo', 'bar', 'bar', 'foo', 'foo', 'bar', 'bar']], names=['letter', 'word'])
+
+df3 = pd.DataFrame({'data1': [3, 2, 4, 3, 2, 4, 3, 2], 'data2': [6, 5, 7, 5, 4, 5, 6, 5]}, index=ix3)
+
+# Group by index labels and take the means and standard deviations for each group
+gp3 = df3.groupby(level=('letter', 'word'))
+
+means = gp3.mean()
+errors = gp3.std()
+
+print(means)
+print(errors)
+
+print(means[Col])
+means[means[Col]<3] = 0
+print(means[Col])
+
+means = gp3.mean()
+means[Col] = means[Col] * 10
+means[Col] = means[Col].astype(int)
+ThresholdD = "30"
+means[means[Col]<float(ThresholdD)] = 0
+print(means[Col])
