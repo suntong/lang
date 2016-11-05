e14d4fc8e0f9e98b8bdee43e3f4651a5bdaec54f
Tong Sun <suntong@cpan.org>
2016-11-05 13:33:32 -0400

    - [!] make gxp_XMLBeautify.go works

--- lang/Go/src/xml/gxp_XMLBeautify.go
+++ lang/Go/src/xml/gxp_XMLBeautify.go
@@ -2,7 +2,7 @@
 // Porgram: gxp_XMLBeautify.go
 // Purpose: goxpath XML Beautify
 // Authors: Tong Sun (c) 2016, All rights reserved
-// Credits: ChrisTrenkamp https://github.com/ChrisTrenkamp/goxpath/issues/5
+// Credits: ChrisTrenkamp https://github.com/ChrisTrenkamp/goxpath/issues/7
 ////////////////////////////////////////////////////////////////////////////
 
 package main
@@ -16,12 +16,15 @@ import (
 
 func main() {
 
-	parseTree := xmltree.MustParseXML(os.Stdin)
+	parseTree := xmltree.MustParseXML(os.Stdin, func(s *xmltree.ParseOptions) {
+		s.Strict = false
+	})
 	goxpath.Marshal(parseTree, os.Stdout)
 }
 
 /*
 
-go run gxp_XMLBeautify.go
+echo "<root><this><is>a</is><test /></this></root>" | go run gxp_XMLBeautify.go
+<root><this><is>a</is><test></test></this></root>
 
 */
