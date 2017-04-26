d659251930b3d23d9700508e240e86767dcdf774
Tong Sun <suntong@cpan.org>
2017-04-26 14:39:02 -0400

    - [*] use the rewritten pkg that allows different series of enums

--- lang/Go/src/ds/EnumsStr1p.go
+++ lang/Go/src/ds/EnumsStr1p.go
@@ -2,7 +2,6 @@
 // Porgram: EnumsStr1p.go
 // Purpose: Go Enum and its string representation lib demo
 // Authors: Tong Sun (c) 2017, All rights reserved
-// Credits: Egon https://groups.google.com/d/msg/golang-nuts/fCdBSRNNUY8/P45qC_03LoAJ
 ////////////////////////////////////////////////////////////////////////////
 
 package main
@@ -11,37 +10,32 @@ import (
 	"fmt"
 
 	enum "github.com/suntong/enum"
-	enum2 "github.com/suntong/enum"
 )
 
 var (
-	Alpha = enum.Ciota("Alpha")
-	Beta  = enum.Ciota("Beta")
+	example enum.Enum
+	Alpha   = example.Iota("Alpha")
+	Beta    = example.Iota("Beta")
 
-	Sunday = enum2.Ciota("Sunday")
-	Monday = enum2.Ciota("Monday")
+	weekday enum.Enum
+	Sunday  = weekday.Iota("Sunday")
+	Monday  = weekday.Iota("Monday")
 )
 
-type Example struct {
-	enum.Enum
-}
-
-type Weekday struct {
-	enum2.Enum
-}
-
 func main() {
-	fmt.Printf("%+v\n", Example{Alpha})
-	fmt.Printf("%+v\n", Example{Beta})
+	fmt.Printf("%s\n", example.String(Alpha))
+	fmt.Printf("%s\n", example.String(Beta))
 	fmt.Println("=======")
 	fmt.Printf("%d\t%d\n", Alpha, Alpha+1)
-	fmt.Printf("%+v\t%+v\n", Example{Beta - 1}, Example{Alpha + 1})
+	fmt.Printf("%s\t%s\n", example.String(Beta-1), example.String(Alpha+1))
 	fmt.Println("=======")
-	if a, ok := enum.Get("Alpha"); ok {
-		fmt.Printf("%d\n", a)
+	if a, ok := example.Get("Alpha"); ok {
+		fmt.Printf("%d: %s\n", a, example.String(a))
 	}
-	if b, ok := enum.Get("Beta"); ok {
-		fmt.Printf("%d: %+v\n", b, Example{b})
+	if b, ok := example.Get("Beta"); ok {
+		fmt.Printf("%d: %+v\n", b, example.String(b))
 	}
-	fmt.Printf("%d:%+v\n", Sunday, Weekday{Sunday})
+
+	fmt.Printf("%d:%s\n", Sunday, weekday.String(Sunday))
+	fmt.Printf("%d:%s\n", Monday, weekday.String(Monday))
 }
