9b8c1c1de77e7c22a6289dafa32b2f10fe76cf2a
Tong Sun <suntong@cpan.org>
2017-05-08 11:34:31 -0400

    - [+] add runeAndString()

--- lang/Go/src/text/Rune.go
+++ lang/Go/src/text/Rune.go
@@ -8,12 +8,16 @@ package main
 
 import (
 	"fmt"
+	"regexp/syntax"
 	"strconv"
 )
 
 func main() {
 	runeFromCode()
+	runeAndString()
 }
+
+////////////////////////////////////////////////////////////////////////////
 
 /*
 
@@ -37,4 +41,39 @@ func runeFromCode() {
 	// hex to int
 	c1, _ := strconv.ParseInt(code10, 16, 16)
 	fmt.Printf("Hello %c\u754c %v %v\n", c1, c1 == '世', "\u4e16" == "世")
+}
+
+////////////////////////////////////////////////////////////////////////////
+// Convert(cast) string to rune and back to string example
+// https://www.socketloop.com/tutorials/golang-convert-cast-string-to-rune-and-back-to-string-example
+
+/*
+
+Tutorial on how how to convert a string to rune type and back to
+string. Dealing with rune can be confusing sometimes because single quotes
+and double quotes can have different meaning in Golang. This example code
+below is pretty straightforward and ... is self explanatory.
+
+*/
+
+func runeAndString() {
+	str := []rune("beta")   // use rune slice
+	acharacter := rune('a') // use single quote, instead of double quote
+	onerune := rune('吃')
+
+	fmt.Printf("%v \n", string(str)) // convert back to string
+
+	fmt.Printf("%v \n", string(acharacter)) // convert back to string
+
+	fmt.Printf("%v \n", string(onerune)) // convert back to string
+
+	// there are times when accessing str is not acceptable because
+	// it is a slice. Therefore, you just have to reference the first
+	// element
+
+	// for example :
+
+	ok := syntax.IsWordChar(str[0]) // won't work without [0]
+
+	fmt.Printf("%v is a word ? : %v \n", string(str), ok)
 }
