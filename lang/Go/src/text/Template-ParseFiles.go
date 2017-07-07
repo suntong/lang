3fef968211f893e2717e0bdef50b6eb27a1b9f53
Tong Sun <suntong@cpan.org>
2017-07-07 18:43:29 -0400

    - [+] add Template-ParseFiles demo

---
+++ lang/Go/src/text/Template-ParseFiles.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"text/template"
+)
+
+type Person string
+
+func main() {
+	t := template.New("Template-ParseFiles.a.tmpl")
+	fmt.Println("Name: ", t.Name())
+	fmt.Printf("Deck: %#v\n", t.Templates())
+	if _, err := t.ParseFiles("Template-ParseFiles.a.tmpl", "Template-ParseFiles.b.tmpl"); err != nil {
+		panic(err)
+	}
+	fmt.Println("Name: ", t.Name())
+	fmt.Printf("Deck: %#v\n", t.Templates())
+	if err := t.Execute(os.Stdout, nil); err != nil {
+		panic(err)
+	}
+
+	if err := t.ExecuteTemplate(os.Stdout, "Template-ParseFiles.b.tmpl", nil); err != nil {
+		panic(err)
+	}
+
+}
