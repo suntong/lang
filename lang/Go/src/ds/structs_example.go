16230bc89d5d39b4d211a63eefc564e575f532e1
Tong Sun <suntong@cpan.org>
2017-08-22 10:58:53 -0400

    - [+] add structs_example.go, not working

---
+++ lang/Go/src/ds/structs_example.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/fatih/structs"
+)
+
+func main() {
+	ExampleNew()
+	ExampleMap()
+	ExampleMaps()
+}
+
+func ExampleNew() {
+	type Server struct {
+		Name    string
+		ID      int32
+		Enabled bool
+	}
+
+	server := &Server{
+		Name:    "Arslan",
+		ID:      123456,
+		Enabled: true,
+	}
+
+	s := structs.New(server)
+
+	fmt.Printf("Name        : %v\n", s.Name())
+	fmt.Printf("Values      : %v\n", s.Values())
+	fmt.Printf("Value of ID : %v\n", s.Field("ID").Value())
+	// Output:
+	// Name        : Server
+	// Values      : [Arslan 123456 true]
+	// Value of ID : 123456
+
+}
+
+func ExampleMap() {
+	type Server struct {
+		Name    string
+		ID      int32
+		Enabled bool
+	}
+
+	s := &Server{
+		Name:    "Arslan",
+		ID:      123456,
+		Enabled: true,
+	}
+
+	m := structs.Map(s)
+
+	fmt.Printf("%#v\n", m["Name"])
+	fmt.Printf("%#v\n", m["ID"])
+	fmt.Printf("%#v\n", m["Enabled"])
+	// Output:
+	// "Arslan"
+	// 123456
+	// true
+
+}
+
+// Map of slice
+func ExampleMaps0() {
+	type Server struct {
+		Name    string
+		ID      int32
+		Enabled bool
+	}
+
+	s := []Server{
+		{
+			Name:    "Arslan",
+			ID:      123456,
+			Enabled: true,
+		},
+		{
+			Name:    "Arslan",
+			ID:      123456,
+			Enabled: true,
+		},
+	}
+
+	// XX: m := structs.Map(s)
+  // panic: not struct
+}
+
+// Map of slice
+func ExampleMaps() {
+	type Server struct {
+		Name    string
+		ID      int32
+		Enabled bool
+	}
+
+	s := struct {
+    Servers     []Server{
+      {[]Server{
+        Name:    "Arslan",
+        ID:      123456,
+        Enabled: true,
+      },
+        {
+          Name:    "Arslan",
+          ID:      123456,
+          Enabled: true,
+        },
+      }
+    }
+  }
+
+	m := structs.Map(s)
+	fmt.Printf("%#v\n", m)
+}
