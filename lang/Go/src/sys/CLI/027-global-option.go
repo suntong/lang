25386223df9a07b76e31c3f4bb36570ee285588e
Tong Sun <suntong@cpan.org>
2017-09-04 10:43:21 -0400

    - [*] 027-global-option uses 027-global-option.json

--- lang/Go/src/sys/CLI/027-global-option.go
+++ lang/Go/src/sys/CLI/027-global-option.go
@@ -18,7 +18,7 @@ func main() {
 // root command
 type rootT struct {
 	cli.Helper
-	Self *rootT `json:"-" cli:"c,config" usage:"config" parser:"jsonfile" dft:"1.json"`
+	Self *rootT `json:"-" cli:"c,config" usage:"config" parser:"jsonfile" dft:"027-global-option.json"`
 	Host string `cli:"H,host" usage:"host addr" dft:"$HOST"`
 	Port int    `cli:"p,port" usage:"listening port"`
 }
