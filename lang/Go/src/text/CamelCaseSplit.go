ef80a01d3faa056269ec067b874f1e0d1c7c4b12
Tong Sun <suntong@cpan.org>
2017-09-06 20:21:03 -0400

    - [*] update CamelCaseSplit.go to include Michael Jones example

--- lang/Go/src/text/CamelCaseSplit.go
+++ lang/Go/src/text/CamelCaseSplit.go
@@ -2,17 +2,25 @@
 // Porgram: CamelCaseSplit.go
 // Purpose: Go splitting CamelCaseWords demo
 // Authors: Tong Sun (c) 2016, All rights reserved
-// Credits: Seth Bunce https://play.golang.org/p/-FM2wC22A0
+// Credits: See blow
 ////////////////////////////////////////////////////////////////////////////
 
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"unicode"
 )
 
 func main() {
+	algorithm1()
+	algorithm2()
+}
+
+//==========================================================================
+// Credits: Seth Bunce https://play.golang.org/p/-FM2wC22A0
+func algorithm1() {
 	s := "fooBarBaz GNU PYTHON Standard"
 	var parts []string
 	start, lastCap := 0, 0
@@ -35,5 +43,37 @@ func main() {
 /*
 
 ["foo" "Bar" "Baz " "GNU " "PYTHON " "Standard"]
+
+*/
+
+//==========================================================================
+// Michael Jones https://play.golang.org/p/AZ7Ptg4HYP
+const testIn = "FooBar3Baz GNU PYTHON Standard"
+const testOut = "Foo Bar3 Baz GNU PYTHON Standard"
+
+func algorithm2() {
+	out := Decamel(testIn)
+	if out != testOut {
+		fmt.Println("error:", out)
+	}
+	fmt.Printf("%q ==> %q\n", testIn, out)
+}
+
+func Decamel(s string) string {
+	var b bytes.Buffer
+	splittable := false
+	for _, v := range s {
+		if splittable && unicode.IsUpper(v) {
+			b.WriteByte(' ')
+		}
+		b.WriteRune(v)
+		splittable = unicode.IsLower(v) || unicode.IsNumber(v)
+	}
+	return b.String()
+}
+
+/*
+
+"FooBar3Baz GNU PYTHON Standard" ==> "Foo Bar3 Baz GNU PYTHON Standard"
 
 */
