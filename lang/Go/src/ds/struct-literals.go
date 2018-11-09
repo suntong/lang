752d28b8f70f4bb3439c557604314b2e81ed9c83
suntong <suntong@cpan.org>
2018-11-09 16:43:46 -0500

    Update struct-literals.go
    add struct Slice

--- lang/Go/src/ds/struct-literals.go
+++ lang/Go/src/ds/struct-literals.go
@@ -1,5 +1,3 @@
-// https://tour.golang.org/moretypes/5
-
 package main
 
 import "fmt"
@@ -16,6 +14,16 @@ var (
 )
 
 func main() {
+	// https://tour.golang.org/moretypes/5
 	fmt.Println(v1, p, v2, v3)
 	// {1 2} &{1 2} {1 0} {0 0}
+
+	// == struct Slice
+	var a []Vertex
+	// append works on nil slices.
+	a = append(a, v1)
+	// the slice grows as needed.
+	a = append(a, v2)
+	fmt.Println(a)
+	// [{1 2} {1 0}]
 }
