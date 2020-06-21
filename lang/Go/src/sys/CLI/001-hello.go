31b3ceb6b495b29431b39cced8b121f9d12bca90
Tong Sun <suntong@cpan.org>
2020-06-21 10:59:38 -0400

    - [+] +To run Go as a scripting language

--- lang/Go/src/sys/CLI/001-hello.go
+++ lang/Go/src/sys/CLI/001-hello.go
@@ -17,3 +17,29 @@ func main() {
 		return nil
 	})
 }
+
+/*
+
+
+To run it as a scripting language
+https://dev.to/ignatk/using-go-as-a-scripting-language-in-linux-4c8c/
+
+One time:
+
+  go get -v -u github.com/erning/gorun
+  sudo mv $GOPATH/bin/gorun /usr/local/bin/gorun
+
+After every reboot:
+
+  echo ':golang:E::go::/usr/local/bin/gorun:OC' | sudo tee /proc/sys/fs/binfmt_misc/register
+
+then,
+
+  $ 001-hello.go -a 75
+  Hello, world! Your age is 75?
+
+  $ 001-hello.go -a 75 --name Tom
+  Hello, Tom! Your age is 75?
+
+
+*/
