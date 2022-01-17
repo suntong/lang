abf6360477dfcf0e4c881d74223785d98b8ef1e3
Tong Sun <suntong@cpan.org>
2022-01-17 12:00:52 -0500

    - [+] add Args to AddCommand

--- lang/Go/src/sys/go-flags/example/add.go
+++ lang/Go/src/sys/go-flags/example/add.go
@@ -6,14 +6,23 @@ import (
 
 type AddCommand struct {
 	All bool `short:"a" long:"all" description:"Add all files"`
+
+	// Example of positional arguments
+	Args struct {
+		ID   string
+		Num  int
+		Rest []string
+	} `positional-args:"yes" required:"yes"`
 }
 
 var addCommand AddCommand
 
 func (x *AddCommand) Execute(args []string) error {
-	fmt.Printf("Adding (all=%v): %#v\n", x.All, args)
+	fmt.Printf("Adding (all=%v): %v %#v\n", x.All, x.Args, args)
 	return nil
 }
+
+
 
 func init() {
 	parser.AddCommand("add",
