5b01b0b467c510b34a71ed4c0cc0c08b33e27e6f
Tong Sun <suntong@cpan.org>
2022-01-17 21:12:10 -0500

    - [#] uncomment imp_publish.go

--- lang/Go/src/sys/go-flags/wireframed/imp_publish.go
+++ lang/Go/src/sys/go-flags/wireframed/imp_publish.go
@@ -14,11 +14,11 @@ import (
 // *** Sub-command: publish ***
 
 // Exec implements the business logic of command `publish`
-// func (x *PublishCommand) Exec(args []string) error {
-// 	fmt.Fprintf(os.Stderr, "Publish the network application\n")
-// 	// fmt.Fprintf(os.Stderr, "Copyright (C) 2022, Myself <me@mine.org>\n\n")
-// 	// fmt.Printf("Doing Publish, with %+v, %+v\n", Opts, args)
-// 	// fmt.Println(x.Dir, x.Suffix, x.Out, x.Args)
-// 	// err := ...
-// 	return nil
-// }
+func (x *PublishCommand) Exec(args []string) error {
+	fmt.Fprintf(os.Stderr, "Publish the network application\n")
+	fmt.Fprintf(os.Stderr, "Copyright (C) 2022, Myself <me@mine.org>\n\n")
+	fmt.Printf("Doing Publish, with %+v, %+v\n", Opts, args)
+	fmt.Println(x.Dir, x.Suffix, x.Out, x.Args)
+	//err := ...
+	return nil
+}
