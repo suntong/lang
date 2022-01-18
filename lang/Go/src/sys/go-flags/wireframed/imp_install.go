b01a439b8a38622d28c0178cffe77feacbbb9454
Tong Sun <suntong@cpan.org>
2022-01-17 22:14:05 -0500

    - [+] refactor imp_ out from cmd_, as-is

---
+++ lang/Go/src/sys/go-flags/wireframed/imp_install.go
@@ -0,0 +1,29 @@
+////////////////////////////////////////////////////////////////////////////
+// Program: redo
+// Purpose: global option redo
+// Authors: Myself <me@mine.org> (c) 2022, All rights reserved
+////////////////////////////////////////////////////////////////////////////
+
+package main
+
+import (
+//	"fmt"
+//	"os"
+
+//	"github.com/go-easygen/go-flags/clis"
+)
+
+// *** Sub-command: install ***
+// Exec implements the business logic of command `install`
+// func (x *InstallCommand) Exec(args []string) error {
+// 	clis.Setup(fmt.Sprintf("%s::%s", progname, "install"), Opts.Verbose)
+// 	fmt.Fprintf(os.Stderr, "Install the network application\n")
+// 	// fmt.Fprintf(os.Stderr, "Copyright (C) 2022, Myself <me@mine.org>\n\n")
+// 	// clis.Verbose(1, "Doing Install, with %+v, %+v\n", Opts, args)
+// 	// fmt.Println(x.Dir, x.Suffix)
+// 	// // err := ...
+// 	// // clis.WarnOn("Doing Install", err)
+// 	// // or,
+// 	// // clis.AbortOn("Doing Install", err)
+// 	return nil
+// }
