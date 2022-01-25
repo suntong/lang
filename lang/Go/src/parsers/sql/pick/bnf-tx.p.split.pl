8905480fc7e03249ba66c9439d8505ab1dc96b18
Tong Sun <suntong@cpan.org>
2022-01-25 00:12:02 -0500

    - [#] bnf-tx.p.split.pl now convert all terms in its rhs to regDefId

--- lang/Go/src/parsers/sql/pick/bnf-tx.p.split.pl
+++ lang/Go/src/parsers/sql/pick/bnf-tx.p.split.pl
@@ -1,10 +1,17 @@
 #!/usr/bin/perl -0777n
 
+sub cookit {
+    my($lhs, $rhs) = @_;
+    # all terms on RHS should be regDefIds
+    $rhs =~ s/([A-Z][a-z]\w+)/"_".lcfirst($&)/ge;
+    return "\n${\( lcfirst($lhs) )} : $rhs"
+}
+
 # split each *e*bnf rule into two parts, lexical & syntax parts
 # to STDOUT and STDERR respectively
 s/(?:\n|^)(\w+)\s*:\s*(.*?;)/
     print STDERR "$1 : ${\( lcfirst($1) )} ;\n\n";
-    "\n${\( lcfirst($1) )} : $2"
+    cookit($1, $2)
 /gsex;
 
 print;
