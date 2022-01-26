03dd1e9ff1dd1ae3570abb39ced7683c356ba9a9
Tong Sun <suntong@cpan.org>
2022-01-25 21:49:58 -0500

    - [#] move bnf-tx.p.pl into bnf-tx.p.split.pl

--- lang/Go/src/parsers/sql/pick/bnf-tx.p.split.pl
+++ lang/Go/src/parsers/sql/pick/bnf-tx.p.split.pl
@@ -26,4 +26,24 @@ my $re = join '|', keys %regDefIds;
 s/(?:\n|^)($re)/_$1/g;
 # echo "foo, bar, AVG, Hex_String_Literal, Time_String, MODULE" | perl -ne 'my %map =("Hex_String_Literal"=>1, "Time_String"=>1,); my $re = join "|", keys %map; s/($re)/_$1/g; print'
 
+# == Transforms from the original bnf-tx.p.pl
+
+
+# fix "Non-xxx" as "Non_xxx"
+s/(Non|Form|of)-/$1_/g;
+
+# every all-CAP words should be split into quoted characters
+s!\b[A-Z_]{2,}!join " ", split //, $&!ge;
+
+# every single character should be quoted
+s/(\s)([^][ ;:|{}])(?=( |\n))/$1'$2'/g;
+
+
+# Repeats
+# 1. { }... is redundant as { } itself means repeating. Thus need to remove
+# 2. word... need to be changed as word {word}
+s/(\{.*?\})\s*\.\.\./$1/gs;
+s/(\S+)\s*\.\.\./$1 {$1}/gs;
+
+
 print;
