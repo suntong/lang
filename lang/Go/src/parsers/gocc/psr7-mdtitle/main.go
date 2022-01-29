842a392642312e10937b5fc001a94c248a378fe1
Tong Sun <suntong@cpan.org>
2022-01-28 22:09:25 -0500

    - [#] add AttribToString to main, but cannot use in bnf

--- lang/Go/src/parsers/gocc/psr7-mdtitle/main.go
+++ lang/Go/src/parsers/gocc/psr7-mdtitle/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/suntong/lang/lang/Go/src/parsers/gocc/psr7-mdtitle/lexer"
 	"github.com/suntong/lang/lang/Go/src/parsers/gocc/psr7-mdtitle/parser"
+	"github.com/suntong/lang/lang/Go/src/parsers/gocc/psr7-mdtitle/token"
 )
 
 func main() {
@@ -18,10 +19,17 @@ func main() {
 	check(err)
 
 	fmt.Printf("ast value: %#+v\n", ast)
+	fmt.Printf("ast Lit value: %s\n", AttribToString(ast))
 }
 
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+type Attrib interface{}
+
+func AttribToString(a Attrib) string {
+	return string(a.(*token.Token).Lit)
 }
