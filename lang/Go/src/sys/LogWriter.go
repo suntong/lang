40b20ffc28a029957ff4efba3ac0e7c7be1f73ab
Tong Sun <suntong@cpan.org>
2022-03-24 17:02:11 -0400

    - [#] update LogWriter.go to log different functions

--- lang/Go/src/sys/LogWriter.go
+++ lang/Go/src/sys/LogWriter.go
@@ -18,24 +18,46 @@ const prog = "LogWriter"
 //const logFmt = "2006-01-02T15:04:05.999Z"
 const logFmt = "15:04:05"
 
+var l = logWriter{"main"}
+
 func (writer logWriter) Write(bytes []byte) (int, error) {
 	return fmt.Printf("%s [%s:%s] %s", time.Now().Format(logFmt),
 		prog, writer.curr, string(bytes))
 }
 
 func main() {
-	l := logWriter{"main"}
 	log.SetFlags(0)
 	log.SetOutput(l)
 	log.Println("This is something being logged!")
+	sub1()
+	log.Println("This is something to be logged in the end.")
+}
+
+func sub1() {
+	defer track("sub1")() // don't forget the extra parentheses
+	log.Println(" working in sub1...")
 	time.Sleep(3 * time.Second) // simulate slow operation by sleeping
-	log.Println("This is something to be logged next.")
+}
+
+func track(curr string) func() {
+	prev := l.curr
+	l.curr = curr
+	log.SetOutput(l) // must!
+	log.Printf("%s:", curr)
+	return func() {
+		log.Printf("%s.", curr)
+		l.curr = prev
+		log.SetOutput(l)
+	}
 }
 
 /*
 
 $ go run LogWriter.go
-16:31:02 [LogWriter] This is something being logged!
-16:31:05 [LogWriter] This is something to be logged next.
+16:50:38 [LogWriter:main] This is something being logged!
+16:50:38 [LogWriter:sub1] sub1:
+16:50:38 [LogWriter:sub1]  working in sub1...
+16:50:41 [LogWriter:sub1] sub1.
+16:50:41 [LogWriter:main] This is something to be logged in the end.
 
 */
