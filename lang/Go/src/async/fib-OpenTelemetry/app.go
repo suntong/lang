7d1bfad942d8f25a0b78bfdd1807f579ea903d95
Tong Sun <suntong@cpan.org>
2022-04-07 17:15:13 -0400

    - [+] add Trace Instrumentation

--- lang/Go/src/async/fib-OpenTelemetry/app.go
+++ lang/Go/src/async/fib-OpenTelemetry/app.go
@@ -5,7 +5,15 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"strconv"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
+
+// name is the Tracer name used to identify this instrumentation library.
+const name = "fib"
 
 // App is a Fibonacci computation application.
 type App struct {
@@ -21,27 +29,48 @@ func NewApp(r io.Reader, l *log.Logger) *App {
 // Run starts polling users for Fibonacci number requests and writes results.
 func (a *App) Run(ctx context.Context) error {
 	for {
-		n, err := a.Poll(ctx)
+		// Each execution of the run loop, we should get a new "root" span and context.
+		newCtx, span := otel.Tracer(name).Start(ctx, "Run")
+
+		n, err := a.Poll(newCtx)
 		if err != nil {
+			span.End()
 			return err
 		}
 
-		a.Write(ctx, n)
+		a.Write(newCtx, n)
+		span.End()
 	}
 }
 
 // Poll asks a user for input and returns the request.
 func (a *App) Poll(ctx context.Context) (uint, error) {
+	_, span := otel.Tracer(name).Start(ctx, "Poll")
+	defer span.End()
+
 	a.l.Print("What Fibonacci number would you like to know: ")
 
 	var n uint
-	_, err := fmt.Fscanf(a.r, "%d\n", &n)
+	_, err := fmt.Fscanf(a.r, "%d", &n)
+
+	// Store n as a string to not overflow an int64.
+	nStr := strconv.FormatUint(uint64(n), 10)
+	span.SetAttributes(attribute.String("request.n", nStr))
+
 	return n, err
 }
 
 // Write writes the n-th Fibonacci number back to the user.
 func (a *App) Write(ctx context.Context, n uint) {
-	f, err := Fibonacci(n)
+	var span trace.Span
+	ctx, span = otel.Tracer(name).Start(ctx, "Write")
+	defer span.End()
+
+	f, err := func(ctx context.Context) (uint64, error) {
+		_, span := otel.Tracer(name).Start(ctx, "Fibonacci")
+		defer span.End()
+		return Fibonacci(n)
+	}(ctx)
 	if err != nil {
 		a.l.Printf("Fibonacci(%d): %v\n", n, err)
 	} else {
