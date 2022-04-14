19a3fad73253a7a80d0356717b3760dab23bc3eb
Tong Sun <suntong@cpan.org>
2022-04-13 22:01:15 -0400

    - [#] update  net httptest ex3 with a real HTTP server

--- lang/Go/src/net/httptest/ex3/server.go
+++ lang/Go/src/net/httptest/ex3/server.go
@@ -1,8 +1,45 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
+
+type Client struct {
+	httpClient *http.Client
+	baseURL    string
+}
+
+type SlothfulMessage struct {
+	Message string `json:"message"`
+}
+
+func NewClient(httpClient *http.Client, baseURL string) Client {
+	return Client{
+		httpClient: httpClient,
+		baseURL:    baseURL,
+	}
+}
+
+func (c *Client) GetSlothfulMessage() (*SlothfulMessage, error) {
+	res, err := c.httpClient.Get(c.baseURL + "/sloth")
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf(
+			"got status code %d", res.StatusCode,
+		)
+	}
+
+	var m SlothfulMessage
+	if err := json.NewDecoder(res.Body).Decode(&m); err != nil {
+		return nil, err
+	}
+	return &m, nil
+}
 
 func handleSlothfulMessage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
--- lang/Go/src/net/httptest/ex3/server_test.go
+++ lang/Go/src/net/httptest/ex3/server_test.go
@@ -23,3 +23,45 @@ func TestHandleSlothfulMessage(t *testing.T) {
 		)
 	}
 }
+
+func TestGetSlothfulMessage(t *testing.T) {
+	router := http.NewServeMux()
+	router.HandleFunc("/sloth", handleSlothfulMessage)
+
+	svr := httptest.NewServer(router)
+	defer svr.Close()
+
+	c := NewClient(http.DefaultClient, svr.URL)
+	m, err := c.GetSlothfulMessage()
+	if err != nil {
+		t.Fatalf("error in GetSlothfulMessage: %v", err)
+	}
+	if m.Message != "Stay slothful!" {
+		t.Errorf(
+			`message %s should contain string "Sloth"`,
+			m.Message,
+		)
+	}
+}
+
+/*
+
+Test your Go web apps with httptest
+https://dev.to/salesforceeng/test-your-go-web-apps-with-httptest-26mc
+
+   Not only does httptest provide us a way to test our handlers with requests and responses, it even provides ways to test your code with a real HTTP server!
+
+   - The client we're making has a GetSlothfulMessage that sends an HTTP request to the /sloth of its baseURL.
+   - Using Go's awesome encoding/json package, the HTTP response body is converted to a SlothfulMessage struct, which is returned if the request and JSON deserialization are successful. We are using json.NewDecoder(res.Body).Decode for reading the response body into our SlothfulMessage struct.
+
+   As you can see, with the httptest package's ResponseRecorder and Server objects, we've got the ability to take the concepts we were already working with for writing tests using the testing package, and then start using functionality to test both receiving and sending HTTP requests. Definitely a must-know package in a Go web developer's toolbelt!
+
+$ go test -v server_test.go server.go
+=== RUN   TestHandleSlothfulMessage
+--- PASS: TestHandleSlothfulMessage (0.00s)
+=== RUN   TestGetSlothfulMessage
+--- PASS: TestGetSlothfulMessage (0.00s)
+PASS
+ok   command-line-arguments     0.007s
+
+*/
