81087cf040e51d6b9deaf3a55493ab174c81d38a
Tong Sun <suntong@cpan.org>
2022-04-15 11:23:35 -0400

    - [+] add PostgreSQL/ex1/ex11

---
+++ lang/Go/src/db/PostgreSQL/ex1/ex11.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "password"
+	dbname   = "postgres"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+}
