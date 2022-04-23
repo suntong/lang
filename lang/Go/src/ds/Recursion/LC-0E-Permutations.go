97377ec856da65fdda8dd8ba9a57f9e739b9a895
Tong Sun <suntong@cpan.org>
2022-04-23 00:06:05 -0400

    - [+] add LC- Permutations from LC-0B-Subsets-2.go (as-is)

---
+++ lang/Go/src/ds/Recursion/LC-0E-Permutations.go
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/permutations/
+// https://medium.com/@CalvinChankf/a-general-approach-for-subsets-combinations-and-permutations-5c8fe3aff0ae
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print(subsets([]int{1, 2, 3}))
+}
+
+func subsets(nums []int) [][]int {
+	res := [][]int{}
+	dfs(nums, []int{}, &res)
+	return res
+}
+
+func dfs(nums, path []int, res *[][]int) {
+	b := make([]int, len(path))
+	copy(b, path)
+	*res = append(*res, b)
+	for i := 0; i < len(nums); i++ {
+		// last n elements of slice
+		dfs(nums[i+1:], append(b, nums[i]), res)
+	}
+}
