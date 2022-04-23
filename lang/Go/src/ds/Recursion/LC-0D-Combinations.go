c844563c9a6ba9208c2698c95f196ddf0034fdfe
Tong Sun <suntong@cpan.org>
2022-04-23 00:03:28 -0400

    - [#] fixed LC- Combinations

--- lang/Go/src/ds/Recursion/LC-0D-Combinations.go
+++ lang/Go/src/ds/Recursion/LC-0D-Combinations.go
@@ -1,4 +1,4 @@
-// https://leetcode.com/problems/subsets/
+// https://leetcode.com/problems/combinations/
 // https://medium.com/@CalvinChankf/a-general-approach-for-subsets-combinations-and-permutations-5c8fe3aff0ae
 
 package main
@@ -8,21 +8,34 @@ import (
 )
 
 func main() {
-	fmt.Print(subsets([]int{1, 2, 3}))
+	fmt.Print(combine(4, 2))
 }
 
-func subsets(nums []int) [][]int {
+func combine(n, k int) [][]int {
 	res := [][]int{}
-	dfs(nums, []int{}, &res)
+	if k < 1 || k > n {
+		return res
+	}
+	// construct the arrary from 1 to n
+	nums := []int{}
+	for i := 0; i < n; i++ {
+		nums = append(nums, i+1)
+	}
+	dfs(nums, []int{}, k, &res)
 	return res
 }
 
-func dfs(nums, path []int, res *[][]int) {
+func dfs(nums, path []int, k int, res *[][]int) {
 	b := make([]int, len(path))
 	copy(b, path)
-	*res = append(*res, b)
+	if len(path) == k {
+		*res = append(*res, b)
+		return
+	}
 	for i := 0; i < len(nums); i++ {
 		// last n elements of slice
-		dfs(nums[i+1:], append(b, nums[i]), res)
+		dfs(nums[i+1:], append(b, nums[i]), k, res)
 	}
 }
+
+// [[1 2] [1 3] [1 4] [2 3] [2 4] [3 4]]
