78a0e2bf7a1861232f2d3db9ffedef7e69d681a2
Tong Sun <suntong@cpan.org>
2022-04-23 00:22:57 -0400

    - [+] add LC- Permutations II from LC-0E-Permutations.go (as-is)

---
+++ lang/Go/src/ds/Recursion/LC-0F-Permutations2.go
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/permutations-ii/
+// https://medium.com/@CalvinChankf/a-general-approach-for-subsets-combinations-and-permutations-5c8fe3aff0ae
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print(permute([]int{1, 2, 3}))
+}
+
+func permute(nums []int) [][]int {
+	res := [][]int{}
+	dfs(nums, []int{}, &res)
+	return res
+}
+
+func dfs(nums, path []int, res *[][]int) {
+	b := make([]int, len(path))
+	copy(b, path)
+	if len(nums) == 0 {
+		*res = append(*res, b)
+	}
+	for i := 0; i < len(nums); i++ {
+		available := []int{}
+		available = append(available, nums[:i]...)
+		// fmt.Println(i, available)
+		available = append(available, nums[i+1:]...)
+		// fmt.Println(i, available)
+		dfs(available, append(b, nums[i]), res)
+	}
+}
