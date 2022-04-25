4e9d0761579701829ca20ceb9666dd2e047618e3
Tong Sun <suntong@cpan.org>
2022-04-24 23:49:35 -0400

    - [#] LC-0G-PermutationK.go now returns the kth permutation sequence

--- lang/Go/src/ds/Recursion/LC-0G-PermutationK.go
+++ lang/Go/src/ds/Recursion/LC-0G-PermutationK.go
@@ -8,20 +8,24 @@ import (
 )
 
 func main() {
-	fmt.Print(permute("123"))
+	fmt.Println(permute("123", 3))  // 213
+	fmt.Println(permute("1234", 9)) // 2314
 }
 
-func permute(nums string) []string {
-	res := []string{}
-	dfs(nums, "", &res)
+func permute(nums string, _k int) string {
+	res, k := "", _k
+	dfs(nums, "", &res, &k)
 	return res
 }
 
-func dfs(nums, path string, res *[]string) {
+func dfs(nums, path string, res *string, k *int) {
 	if len(nums) == 0 {
-		*res = append(*res, path)
+		*k--
+		if *k == 0 {
+			*res = path
+		}
 	}
 	for i := 0; i < len(nums); i++ {
-		dfs(nums[:i]+nums[i+1:], path+string(nums[i]), res)
+		dfs(nums[:i]+nums[i+1:], path+string(nums[i]), res, k)
 	}
 }
