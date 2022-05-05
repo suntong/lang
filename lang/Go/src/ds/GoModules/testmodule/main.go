8da7f0059486e604718f243071cb10112c516673
Tong Sun <suntong@cpan.org>
2022-05-05 13:18:19 -0400

    - [+] add mypackage.Double()

--- lang/Go/src/ds/GoModules/testmodule/main.go
+++ lang/Go/src/ds/GoModules/testmodule/main.go
@@ -10,4 +10,6 @@ func main() {
 	fmt.Println("Hello, Modules!")
 
 	mypackage.PrintHello()
+
+	fmt.Println(mypackage.Double(3))
 }
--- lang/Go/src/ds/GoModules/testmodule/mypackage/mypackage.go
+++ lang/Go/src/ds/GoModules/testmodule/mypackage/mypackage.go
@@ -5,3 +5,7 @@ import "fmt"
 func PrintHello() {
 	fmt.Println("Hello, Modules! This is mypackage speaking!")
 }
+
+func Double(i int) int {
+	return i * 2
+}
