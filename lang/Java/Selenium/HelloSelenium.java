1ab4a457a98a4746c5fa47dc1c8aec10b8ae58e2
Tong Sun <suntong@cpan.org>
2022-05-13 23:34:16 -0400

    - [!] fixed HelloSelenium.java

--- lang/Java/Selenium/HelloSelenium.java
+++ lang/Java/Selenium/HelloSelenium.java
@@ -1,19 +1,28 @@
 // https://www.selenium.dev/documentation/webdriver/getting_started/first_script/
 
+//import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class HelloSelenium {
   public static void main(String[] args) {
-    driver = new ChromeDriver();
+    System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+    ChromeOptions chromeOptions = new ChromeOptions();
+    chromeOptions.addArguments("--headless");
+    chromeOptions.addArguments("--no-sandbox");
+
+    WebDriver driver = new ChromeDriver(chromeOptions);
 
     driver.get("https://google.com");
 
     driver.getTitle(); // => "Google"
 
-    driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+    driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
 
     WebElement searchBox = driver.findElement(By.name("q"));
     WebElement searchButton = driver.findElement(By.name("btnK"));
