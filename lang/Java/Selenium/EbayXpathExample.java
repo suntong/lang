68685cac0471dd152a747face76e1d4dbd744498
Tong Sun <suntong@cpan.org>
2022-05-14 00:31:58 -0400

    - [!] fixed EbayXpathExample.java

--- lang/Java/Selenium/EbayXpathExample.java
+++ lang/Java/Selenium/EbayXpathExample.java
@@ -1,20 +1,26 @@
 // https://www.browserstack.com/guide/understanding-selenium-timeouts
 
 import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class EbayXpathExample {
   public static void main(String[] args) {
+    System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+    ChromeOptions chromeOptions = new ChromeOptions();
+    chromeOptions.addArguments("--headless");
+    chromeOptions.addArguments("--no-sandbox");
 
-    System.setProperty("webdriver.chrome.driver", "Path of Chrome Driver");
-    WebDriver driver = new ChromeDriver();
+    WebDriver driver = new ChromeDriver(chromeOptions);
+
     driver.get("https://www.ebay.com/");
 
-    // Implicit wait timeout for 20seconds
-    driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    // Implicit wait timeout for 2 seconds
+    driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 
     driver.findElement(By.xpath("//input[@id='gh-ac']")).sendKeys("Mobile");
 
