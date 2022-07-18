460b00afad01c8bdcb2607b3e5afe40384e5fe81
Tong Sun <suntong@cpan.org>
2022-07-18 00:06:44 -0400

    - [#] simplify for 0205.Isomorphic-Strings

--- lang/Go/src/ds/LeetCode/0205.Isomorphic-Strings/code.go
+++ lang/Go/src/ds/LeetCode/0205.Isomorphic-Strings/code.go
@@ -1,28 +1,18 @@
 package leetcode
 
 func isIsomorphic(s string, t string) bool {
-	strList := []byte(t)
-	patternByte := []byte(s)
-	if (s == "" && t != "") || (len(patternByte) != len(strList)) {
+	if len(s) != len(t) {
 		return false
 	}
 
-	pMap := map[byte]byte{}
+	tByte := []byte(t)
+	sByte := []byte(s)
 	sMap := map[byte]byte{}
-	for index, b := range patternByte {
-		if _, ok := pMap[b]; !ok {
-			if _, ok = sMap[strList[index]]; !ok {
-				pMap[b] = strList[index]
-				sMap[strList[index]] = b
-			} else {
-				if sMap[strList[index]] != b {
-					return false
-				}
-			}
-		} else {
-			if pMap[b] != strList[index] {
-				return false
-			}
+	for i, b := range sByte {
+		if _, ok := sMap[b]; !ok {
+			sMap[b] = tByte[i]
+		} else if sMap[b] != tByte[i] {
+			return false
 		}
 	}
 	return true
