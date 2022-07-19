86427202afb4fb376d5ec5e218189b809e92b006
Tong Sun <suntong@cpan.org>
2022-07-19 08:37:43 -0400

    - [!] naive approach fixed

--- lang/Go/src/ds/LeetCode/0283.Move-Zeroes/code.go
+++ lang/Go/src/ds/LeetCode/0283.Move-Zeroes/code.go
@@ -1,17 +1,16 @@
 package leetcode
 
 func moveZeroes(nums []int) {
-	if len(nums) == 0 {
+	if len(nums) <= 1 {
 		return
 	}
 	j := len(nums) - 1
-	for ; j > 0; j-- {
-		if nums[j] != 0 {
-			break
-		}
-	}
-
 	for i := 0; i < j; i++ {
+		for ; j > i; j-- {
+			if nums[j] != 0 {
+				break
+			}
+		}
 		if nums[i] == 0 {
 			nums[i], nums[j] = nums[j], nums[i]
 			j--
