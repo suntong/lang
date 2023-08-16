8565841ddc0b96ac1824c82f7319820425a8c4f3
Tong Sun <suntong@cpan.org>
2023-08-16 16:04:13 -0400

    - [+] add case of mixture of field:value and value elements in struct literal

--- lang/Go/src/ds/StructAnonymousFieldDef1.go
+++ lang/Go/src/ds/StructAnonymousFieldDef1.go
@@ -17,7 +17,7 @@ type Truck struct {
 	Payload string
 }
 
-func main() {
+func test1() {
 	{
 		v := Vehicle{4}
 		fmt.Printf("Hello, %#v\n", v)
@@ -41,10 +41,42 @@ func main() {
 	}
 	{
 		t1 := Truck{Vehicle{4}, "1.5T"}
-		fmt.Printf("Hello, %#v\n", t1)
+		fmt.Printf("%d-wheelTruck, %#v\n", t1.Vehicle.wheelCount, t1)
 
 		v := Vehicle{6}
 		t2 := Truck{v, "2.5T"}
-		fmt.Printf("Hello, %#v\n", t2)
+		fmt.Printf("%d-wheelTruck, %+v\n", t2.wheelCount, t2)
 	}
+	{
+		// unknown field 'wheelCount' in struct literal
+		// mixture of field:value and value elements in struct literal
+		// t1 := Truck{wheelCount: 3, "0.5T"}
+		// t1 := Car{wheelCount: 3, "0.5T"}
+		// fmt.Printf("Three-wheel car, %#v\n", t1)
+	}
+}
+
+type T1 struct {
+	T1_Text string
+}
+
+type T2 struct {
+	T2_Text string
+	T1
+}
+
+func test2() {
+	// unknown field 'T1_Text' in struct literal of type T2
+	// t2 := T2{T2_Text: "Test2", T1_Text: "Test1"}
+
+	// mixture of field:value and value elements in struct literal
+	// X: t := T2{T2_Text: "Test", T1{T1_Text: "Test"}}
+	// https://stackoverflow.com/a/47379416/2125837
+	t2 := T2{T2_Text: "Test2", T1: T1{T1_Text: "Test1"}}
+	fmt.Println(t2)
+}
+
+func main() {
+	test1()
+	test2()
 }
