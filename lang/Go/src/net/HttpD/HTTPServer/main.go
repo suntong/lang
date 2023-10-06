4a7cc952c9cfa1ac367bddb2acc615ff7c4fe453
Tong Sun <suntong@cpan.org>
2023-10-06 14:48:42 -0400

    - [+] add simple web server

---
+++ lang/Go/src/net/HttpD/HTTPServer/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(w, "hello\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func main() {
+
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+
+	// server start
+	log.Print("Listening...")
+	log.Fatal("ListenAndServe: ", http.ListenAndServe(":8899", nil))
+
+}
