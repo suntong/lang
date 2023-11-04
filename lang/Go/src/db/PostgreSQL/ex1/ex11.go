d273492681b6a11f352e840a66e98ff528bfe71c
Tong Sun <suntong@cpan.org>
2023-11-04 15:20:10 -0400

    - [#] get PostgreSQL connection from env

--- lang/Go/src/db/PostgreSQL/ex1/ex11.go
+++ lang/Go/src/db/PostgreSQL/ex1/ex11.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/lib/pq"
 )
@@ -20,6 +21,16 @@ func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
+	// URL for PostgreSQL connection.
+	psqlInfo = fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_SSL_MODE"),
+	)
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
