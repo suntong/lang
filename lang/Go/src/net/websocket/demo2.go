07f01a286f90ea7d283e0eb45ea3d840d3322202
AntonioSun <asun@ats.l5.ca>
2024-05-31 20:56:48 -0400

    - [+] websocket demo2 add timing control

--- lang/Go/src/net/websocket/demo2.go
+++ lang/Go/src/net/websocket/demo2.go
@@ -49,16 +49,19 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(2)
 
+	// Channel to signal when to send the next message
+	sendChan := make(chan struct{})
+
 	// Start goroutine for sending messages
 	go func() {
 		defer wg.Done()
-		sendMessages(ctx, conn)
+		sendMessages(ctx, conn, sendChan)
 	}()
 
 	// Start goroutine for receiving messages
 	go func() {
 		defer wg.Done()
-		receiveMessages(ctx, conn)
+		receiveMessages(ctx, conn, sendChan)
 	}()
 
 	// Wait for both goroutines to finish
@@ -69,10 +72,11 @@ func main() {
 	log.Println("Connection closed gracefully")
 }
 
-func sendMessages(ctx context.Context, conn *websocket.Conn) {
+func sendMessages(ctx context.Context, conn *websocket.Conn, sendChan chan struct{}) {
 	ticker := time.NewTicker(5 * time.Second)
 	defer ticker.Stop()
 
+	startTime := time.Now()
 	for {
 		select {
 		case <-ctx.Done():
@@ -82,25 +86,28 @@ func sendMessages(ctx context.Context, conn *websocket.Conn) {
 			message := fmt.Sprintf("Hello, the time is %s", t)
 			sendCtx, sendCancel := context.WithTimeout(ctx, webSocketTimeout*time.Second)
 			defer sendCancel()
+			startTime = time.Now() // Record start time
 			err := wsjson.Write(sendCtx, conn, message)
 			if err != nil {
 				log.Printf("Failed to send message: %v", err)
 				return
 			}
 			log.Printf("Sent: %s", message)
+			// Notify the receive goroutine to start receiving, after startTime
+			sendChan <- struct{}{}
+			log.Printf("Time from send to receive: %v", time.Since(startTime))
 		}
 	}
 }
 
-func receiveMessages(ctx context.Context, conn *websocket.Conn) {
+func receiveMessages(ctx context.Context, conn *websocket.Conn, sendChan chan struct{}) {
 	for {
 		select {
 		case <-ctx.Done():
 			log.Println("Context cancelled, stopping receiveMessages goroutine")
 			return
-		default:
-			// err := wsjson.Read(ctx, conn, &message)
-			recvCtx, recvCancel := context.WithTimeout(ctx, webSocketTimeout*2*time.Second)
+		case <-sendChan:
+			recvCtx, recvCancel := context.WithTimeout(ctx, webSocketTimeout*time.Second)
 			defer recvCancel()
 			var message string
 			err := wsjson.Read(recvCtx, conn, &message)
@@ -124,8 +131,10 @@ go build -o demo2 demo2.go
 
 $ demo2
 2024/05/31 19:36:22 Sent: Hello, the time is 2024-05-31 19:36:22.490033 -0400 EDT m=+5.276632459
+2024/05/31 19:36:22 Time from send to receive: 572.333µs
 2024/05/31 19:36:22 Received: Hello, the time is 2024-05-31 19:36:22.490033 -0400 EDT m=+5.276632459
 2024/05/31 19:36:27 Sent: Hello, the time is 2024-05-31 19:36:27.492419 -0400 EDT m=+10.278983542
+2024/05/31 19:36:27 Time from send to receive: 377.625µs
 2024/05/31 19:36:27 Received: Hello, the time is 2024-05-31 19:36:27.492419 -0400 EDT m=+10.278983542
 2024/05/31 19:36:32 Sent: Hello, the time is 2024-05-31 19:36:32.489689 -0400 EDT m=+15.276219376
 2024/05/31 19:36:32 Received: Hello, the time is 2024-05-31 19:36:32.489689 -0400 EDT m=+15.276219376
