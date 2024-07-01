28d7e00d4c2c14a76d7468512739763204fe8aaf
AntonioSun <asun@ats.l5.ca>
2024-07-01 11:12:19 -0400

    - [+] convert from halfwidth to fullwidth

--- lang/Perl/sokoban-coord.pl
+++ lang/Perl/sokoban-coord.pl
@@ -1,22 +1,34 @@
 #!/usr/bin/perl -l
 use strict;
 use warnings;
+use open ':std', ':encoding(UTF-8)';
 
 my $letter = 'A';
+my $fill = @ARGV >= 1;
+
+sub AZaz {
+    my ($letter) = @_;
+    if ($letter eq "AA") { $letter = "a"; }
+    $letter
+}
 
 sub sokoban_puzzle_to_coordinate {
     # http://sokobano.de/wiki/index.php?title=Level_format
     my ($str) = @_;
 
-    $str =~ s{([ \$\@\*\.\+\-]+)
-        }{
-        my $replacement = join '', map { 
-            if ($letter++ eq "AA") { $letter = "a"; }
-            $letter;
-            } 1 .. length $&;
-        "$replacement";
-    }gex;
+    if ($fill) {
+        $str =~ s{([ \$\@\*\.\+\-]+)
+            }{
+            my $replacement = join '', map {
+                AZaz($letter++);
+                } 1 .. length $&;
+            "$replacement";
+        }gex;
+    }
 
+    $str =~ s{.}{
+        chr(ord($&) + 0xfee0);
+        }ge;
     return $str;
 }
 
