ddad2d8d23ef50958f63171ca2ee6acc43462685
Tong Sun <suntong@cpan.org>
2024-09-06 18:33:22 -0400

    - [#] udpate Basename.go demos

--- lang/Go/src/sys/Basename.go
+++ lang/Go/src/sys/Basename.go
@@ -8,4 +8,17 @@ import (
 func main() {
 	fmt.Println(filepath.Base("/a/b.c"))
 	fmt.Println(filepath.Base(`C:\Program Files\Microsoft Visual Studio 9\Common7\IDE\devenv.exe`))
+	fmt.Println(filepath.Base("."))
+	abs, _ := filepath.Abs(".")
+	fmt.Println(filepath.Base(abs))
 }
+
+/*
+
+$ go run Basename.go
+b.c
+C:\Program Files\Microsoft Visual Studio 9\Common7\IDE\devenv.exe
+.
+sys
+
+*/
