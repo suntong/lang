825923ee4c28717a0bb14641b3919f46d351df99
Tong Sun <suntong@cpan.org>
2025-11-14 08:24:37 -0500

    - [+] add log-to-stderr.go

---
+++ lang/Go/src/io/log-to-stderr.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "log"
+)
+
+func main() {
+    // This uses the default logger, which writes to os.Stderr
+    log.Printf("This message goes to standard error (stderr).")
+}
